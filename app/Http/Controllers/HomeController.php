d9490f4f078c726879c2051a61fa470fc3761a43
Abdoug <ougouagabderrahmane@gmail.com>
2020-02-01 23:11:46 +0100

    setting everything before implemeting pusher

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        // Select all users except the logged in
+        $users = User::where('id', '!=', Auth::id())->get();
+
+        return view('home', compact('users'));
+    }
+
+    public function messages($user_id)
+    {
+
+        $current_user = Auth::id();
+        // Get the messages where from === Auth::id() && to === $user_id OR from === $user_id && to === Auth::id()
+        $messages = Message::where(function ($query) use ($current_user, $user_id) {
+            $query->where('from', $current_user)->where('to', $user_id);
+        })->oRwhere(function ($query) use ($current_user, $user_id) {
+            $query->where('from', $user_id)->where('to', $current_user);
+        })->get();
+
+        return view('messages.index', compact('messages'));
+    }
+
+    public function sendMessage(Request $request)
+    {
+        $from = Auth::id();
+        $to = $request->receiver_id;
+        $message = $request->message;
+        $is_read = 0;
+
+        $new_message = new Message();
+        $new_message->from = $from;
+        $new_message->to = $to;
+        $new_message->message = $message;
+        $new_message->is_read = $is_read;
+
+        $new_message->save();
+    }
+}
